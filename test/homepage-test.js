b0feafceb80e47806e1b09a7952b057cd0f2d404
PraveenKumar Natarajan <praveenkumar.natarajan@flatironssolutions.com>
2020-04-16 20:18:20 +0530

    Section 38: Handling Browser Tabs

--- test/homepage-test.js
+++ test/homepage-test.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 
 describe("Twst Webdriver University HomePage", () => {
   it("Test Webdriver university Homepage Title", () => {
+    browser.maximizeWindow()
     browser.url('./');
     const title = browser.getTitle();
     assert.strictEqual(title,'WebDriverUniversity.com')
